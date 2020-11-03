f7660ef6aa73aa03a1d81741287e545f1089da0d
prithviraj-maurya <mouryaprithviraj@gmail.com>
2020-11-03 17:38:08 +0530

    adding changes for add expense screen

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AddExpenseComponent } from './components/add-expense/add-expense.component';
 import { IonicModule } from '@ionic/angular';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 
@@ -9,7 +10,8 @@ import { IonicModule } from '@ionic/angular';
   declarations: [AddExpenseComponent],
   imports: [
     CommonModule,
-    IonicModule
+    IonicModule,
+    ReactiveFormsModule
   ],
   entryComponents: [
     AddExpenseComponent
