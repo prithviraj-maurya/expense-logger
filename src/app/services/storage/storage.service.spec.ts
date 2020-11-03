c703a66648fe3fc711cf909096ed744055a96320
prithviraj-maurya <mouryaprithviraj@gmail.com>
2020-11-03 16:15:31 +0530

    Addint test case for storage service

--- src/app/services/storage/storage.service.spec.ts
+++ src/app/services/storage/storage.service.spec.ts
@@ -6,11 +6,23 @@ describe('StorageService', () => {
   let service: StorageService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        StorageService
+      ]
+    });
     service = TestBed.inject(StorageService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should save key and value to storage', () => {
+    const obj = { value: 'test' }
+    service.setObject('key', obj);
+    const result = service.getObject('key').then((result: { value: any }) => {
+      expect(result).toEqual(obj);
+    });
   });
 });
